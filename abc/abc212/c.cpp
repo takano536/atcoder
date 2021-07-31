4071cc3877b154e5c191c9a487b3bdb680fa8971
takano536 <ohnishi.takanori@gmail.com>
2021-07-31 22:57:21 +0900

    full search

---
+++ abc/abc212/c.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    vector<pair<int, char>> all;
+    rep (i, n)
+        cin >> a[i], all.push_back(make_pair(a[i], 'a'));
+    rep (i, m)
+        cin >> b[i], all.push_back(make_pair(b[i], 'b'));
+
+    sort(all.begin(), all.end());
+    int i = 0, j = 0;
+    vector<int> index;
+    rep (i, n + m - 1)
+        if (all[i].second != all[i + 1].second)
+            index.push_back(i);
+    int ans = 1e9;
+    for (int i : index)
+        ans = min(abs(all[i].first - all[i + 1].first), ans);
+
+    cout << ans << endl;
+    return 0;
+}
