27334b919e2ec348e54dcd4e27b4b221164ba9fd
takano536 <ohnishi.takanori@gmail.com>
2021-08-08 22:56:21 +0900

    coordinate_compression

--- abc/abc213/c.cpp
+++ abc/abc213/c.cpp
@@ -9,26 +9,22 @@ int main()
 {
     int h, w, n;
     cin >> h >> w >> n;
-    vector<pair<int, int>> a(n), b(n);
+    vector<int> a(n), b(n);
     rep (i, n)
-        cin >> a[i].first >> b[i].first, a[i].second = i, b[i].second = i;
+        cin >> a[i] >> b[i];
 
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    vector<pair<int, int>> ans(n);
-    int a_now = a[0].first, b_now = b[0].first;
-    int a_cnt = 1, b_cnt = 1;
+    vector<int> sorted_a = a, sorted_b = b;
+    sort(sorted_a.begin(), sorted_a.end());
+    sort(sorted_b.begin(), sorted_b.end());
+    sorted_a.erase(unique(sorted_a.begin(), sorted_a.end()), sorted_a.end());
+    sorted_b.erase(unique(sorted_b.begin(), sorted_b.end()), sorted_b.end());
     rep (i, n)
     {
-        if (a[i].first != a_now)
-            a_cnt++, a_now = a[i].first;
-        if (b[i].first != b_now)
-            b_cnt++, b_now = b[i].first;
-        ans[a[i].second].first = a_cnt;
-        ans[b[i].second].second = b_cnt;
+        int ans_a, ans_b;
+        ans_a = lower_bound(sorted_a.begin(), sorted_a.end(), a[i]) - sorted_a.begin();
+        ans_b = lower_bound(sorted_b.begin(), sorted_b.end(), b[i]) - sorted_b.begin();
+        cout << ans_a + 1 << ' ' << ans_b + 1 << endl;
     }
 
-    rep (i, n)
-        cout << ans[i].first << ' ' << ans[i].second << endl;
     return 0;
 }
