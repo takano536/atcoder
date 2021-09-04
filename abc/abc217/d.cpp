ed0a32f9b7ce044aaae47160c9adc85174e54f23
takano536 <ohnishi.takanori@gmail.com>
2021-09-04 23:02:25 +0900

    lower_bound

--- abc/abc217/d.cpp
+++ abc/abc217/d.cpp
@@ -7,21 +7,21 @@ using ll = long long;
 
 int main()
 {
-	int l, q;
-	cin >> l >> q;
-	vector<int> div;
-	div.push_back(0);
-	div.push_back(l);
-	rep (i, q)
-	{
-		int c, x;
-		cin >> c >> x;
-		auto iter = lower_bound(div.begin(), div.end(), x);
-		if (c == 1)
-			div.insert(iter, x);
-		else
-			cout << *iter - *(iter - 1) << endl;
-	}
+    int l, q;
+    cin >> l >> q;
+    vector<int> div;
+    div.push_back(0);
+    div.push_back(l);
+    rep (i, q)
+    {
+        int c, x;
+        cin >> c >> x;
+        auto iter = lower_bound(div.begin(), div.end(), x);
+        if (c == 1)
+            div.insert(iter, x);
+        else
+            cout << *iter - *(iter - 1) << endl;
+    }
 
-	return 0;
+    return 0;
 }
