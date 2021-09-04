292898eff5bcd651a3c8444ece2b15bd317135a5
takano536 <ohnishi.takanori@gmail.com>
2021-09-04 23:02:32 +0900

    priority_queue

--- abc/abc217/e.cpp
+++ abc/abc217/e.cpp
@@ -7,34 +7,34 @@ using ll = long long;
 
 int main()
 {
-	int q;
-	cin >> q;
-	queue<int> que;
-	priority_queue<int, vector<int>, greater<int>> priority_que;
-	rep (i, q)
-	{
-		int c;
-		cin >> c;
-		switch (c)
-		{
-			case 1:
-				int x;
-				cin >> x;
-				que.push(x);
-				break;
+    int q;
+    cin >> q;
+    queue<int> que;
+    priority_queue<int, vector<int>, greater<int>> priority_que;
+    rep (i, q)
+    {
+        int c;
+        cin >> c;
+        switch (c)
+        {
+            case 1:
+                int x;
+                cin >> x;
+                que.push(x);
+                break;
 
-			case 2:
-				if (!priority_que.empty())
-					cout << priority_que.top() << endl, priority_que.pop();
-				else
-					cout << que.front() << endl, que.pop();
-				break;
+            case 2:
+                if (!priority_que.empty())
+                    cout << priority_que.top() << endl, priority_que.pop();
+                else
+                    cout << que.front() << endl, que.pop();
+                break;
 
-			case 3:
-				while (!que.empty())
-					priority_que.push(que.front()), que.pop();
-				break;
-		}
-	}
-	return 0;
+            case 3:
+                while (!que.empty())
+                    priority_que.push(que.front()), que.pop();
+                break;
+        }
+    }
+    return 0;
 }
