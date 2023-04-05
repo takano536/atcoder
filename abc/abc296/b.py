e4f1ee5e011831696a8b932d711b27a43ca5ea44
takano536 <ohnishi.takanori@gmail.com>
2023-04-06 04:22:49 +0900

    io

--- abc/abc296/b.py
+++ abc/abc296/b.py
@@ -1,13 +1,15 @@
 s = [input() for _ in range(8)]
 
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+alphabet, num = str(), str()
 
 for i, r in enumerate(s):
-    if not '*' in r:
+    if '*' not in r:
         continue
     num = str(len(s) - i)
     for j, c in enumerate(r):
         if c == '*':
             alphabet = alphabets[j]
 
-print(alphabet + num)
+ans = "".join([alphabet, num])
+print(ans)
