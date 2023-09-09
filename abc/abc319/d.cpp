66c3a9c9a73a43a52c0bf793b81f2eb75b3f6d6a
takano536 <ohnishi.takanori@gmail.com>
2023-09-09 22:54:42 +0900

    binary search

---
+++ abc/abc319/d.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> l(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> l[i];
+    }
+
+    long long ng = 0;
+    long long ok = 1e18;
+    while (ok - ng > 1) {
+        long long mid = (ok + ng) / 2;
+        long long line = 1;
+        long long width = 0;
+        for (int i = 0; i < n - 1; i++) {
+            width += l[i];
+            width++;
+            if (width + l[i + 1] > mid) {
+                line++;
+                width = 0;
+            }
+        }
+        if (line <= m) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    std::cout << std::max(ok, static_cast<long long>(*std::max_element(l.begin(), l.end()))) << std::endl;
+    return 0;
+}
