7f5f8ac8e89bba8b7890379eb9db1c3c2d433581
takano536 <ohnishi.takanori@gmail.com>
2024-01-06 23:20:56 +0900

    io

---
+++ abc/abc335/b.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n + 1):
+    for j in range(n + 1):
+        for k in range(n + 1):
+            if i + j + k > n:
+                continue
+            print(f'{i} {j} {k}')
