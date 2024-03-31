a29cf40d6962c671cdb4642361a29658b853651c
takano536 <ohnishi.takanori@gmail.com>
2024-03-31 17:06:33 +0900

    io

--- abc/abc347/a.py
+++ abc/abc347/a.py
@@ -1,5 +1,3 @@
 n, k = map(int, input().split())
-a = [int(num) // k for num in input().split() if int(num) % k == 0]
-for ans in a:
-    print(ans, end=' ')
-print()
+ans = [int(num) // k for num in input().split() if int(num) % k == 0]
+print(*ans)
