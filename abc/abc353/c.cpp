4efb6a98e52bc5ac054122ed228e6124f6bc7197
takano536 <ohnishi.takanori@gmail.com>
2024-05-12 16:08:19 +0900

    cumsum, binary search

--- abc/abc353/c.cpp
+++ abc/abc353/c.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
 #include <vector>
 
 int main() {
@@ -17,16 +16,16 @@ int main() {
     std::vector<int> sorted_a = a;
     std::ranges::sort(sorted_a);
 
-    std::vector<long long> a_cumsum(n + 1);
+    std::vector<long long> cumsum(n + 1);
     for (int i = 0; i < n; i++) {
-        a_cumsum[i + 1] = a_cumsum[i] + a[i];
+        cumsum[i + 1] = cumsum[i] + a[i];
     }
 
     long long ans = 0;
     long long cnt = 0;
     for (int i = 0; i < n; i++) {
         auto dist = std::distance(sorted_a.begin(), std::ranges::lower_bound(sorted_a, MOD - a[i]));
-        ans += a_cumsum[n] - a_cumsum[i + 1];
+        ans += cumsum[n] - cumsum[i + 1];
         ans += static_cast<long long>(a[i]) * (n - i - 1);
         cnt += n - dist;
         if (a[i] >= MOD - a[i]) cnt--;
