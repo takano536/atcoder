bd6bf4e0344672150bfb059f580a41788aa3d416
takano536 <ohnishi.takanori@gmail.com>
2024-06-05 15:46:38 +0900

    io

---
+++ abc/abc275/a.py
@@ -0,0 +1,3 @@
+n = input()
+h = list(map(int, input().split()))
+print(h.index(max(h)) + 1)
---
+++ abc/abc275/b.py
@@ -0,0 +1,3 @@
+MOD = 998244353
+a, b, c, d, e, f = map(int, input().split())
+print((a * b * c - d * e * f) % MOD)
