e4e3900b1c23a84f85941d949a06d82a42e713ab
takao536 <ohnishi.takanori@gmail.com>
2024-11-11 15:55:21 +0900

    greedy

--- abc/abc379/c.cpp
+++ abc/abc379/c.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <set>
 #include <utility>
 #include <vector>
 
@@ -9,35 +8,32 @@ int main() {
     std::cin >> n >> m;
 
     std::vector<std::pair<int, int>> stones(m);
-    long long cnts = 0;
     for (auto &[x, _] : stones) std::cin >> x, x--;
-    for (auto &[_, a] : stones) std::cin >> a, cnts += a;
+    for (auto &[_, a] : stones) std::cin >> a;
+    stones.emplace_back(-1, 1);
+    stones.emplace_back(n, 1);
 
     std::ranges::sort(stones);
 
-    if (cnts != n || stones[0].first != 0) {
-        std::cout << -1 << std::endl;
-        return 0;
-    }
-
-    std::set<int> used_cells;
-    for (const auto &[x, _] : stones) used_cells.insert(x);
-    used_cells.insert(n);
-
     long long ans = 0;
-    auto iter = used_cells.begin();
-    for (int i = 0; i < m; i++) {
-        const auto &[_, curr_cnt] = stones[i];
-        int diff = *std::next(iter) - *iter;
-        int empty_cnt = diff - 1;
-        if (diff > curr_cnt || (i == m - 1 && diff != curr_cnt)) {
+    for (int i = 0; i <= m; i++) {
+        const auto &[curr_coord, curr_num] = stones[i];
+        auto &[next_coord, next_num] = stones[i + 1];
+
+        const int available_num = curr_num - 1;
+        const int blank_num = next_coord - curr_coord - 1;
+
+        if (available_num < blank_num) {
             std::cout << -1 << std::endl;
             return 0;
         }
-        ans += static_cast<long long>(empty_cnt) * (empty_cnt + 1) / 2;
-        ans += static_cast<long long>(curr_cnt - empty_cnt - 1) * diff;
-        if (i != m - 1) stones[i + 1].second += curr_cnt - diff;
-        iter++;
+        ans += static_cast<long long>(blank_num + 1) * blank_num / 2;
+
+        const int excess_num = available_num - blank_num;
+        next_num += excess_num;
+
+        const int cost = next_coord - curr_coord;
+        ans += static_cast<long long>(excess_num) * cost;
     }
 
     std::cout << ans << std::endl;
