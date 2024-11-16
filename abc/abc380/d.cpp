69aa897fc060eb9e5def1c7f613af6484035a6bb
takao536 <ohnishi.takanori@gmail.com>
2024-11-17 00:29:36 +0900

    cyclic, bit, popcount

--- abc/abc380/d.cpp
+++ abc/abc380/d.cpp
@@ -7,8 +7,8 @@ int main() {
     std::string s;
     std::cin >> s;
 
-    std::string swapped_s = s;
-    for (auto &c : swapped_s) c = (std::isupper(c) ? std::tolower(c) : std::toupper(c));
+    std::string flipped_s = s;
+    for (auto &c : flipped_s) c = (std::isupper(c) ? std::tolower(c) : std::toupper(c));
 
     const int n = s.size();
 
@@ -20,9 +20,9 @@ int main() {
 
     for (const auto &ki : k) {
         long long group = (ki + n - 1) / n - 1;
-        bool is_swapped = std::bitset<64>(group).count() % 2;
+        bool is_flipped = std::bitset<64>(group).count() % 2;
         int idx = (ki - 1) % n;
-        std::cout << (is_swapped ? swapped_s[idx] : s[idx]) << ' ';
+        std::cout << (is_flipped ? flipped_s[idx] : s[idx]) << ' ';
     }
     std::cout << std::endl;
     return 0;
