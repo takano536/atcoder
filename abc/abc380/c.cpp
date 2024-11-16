6d09c28d341abd3886cacd5eba397bb3f52b3a6a
takao536 <ohnishi.takanori@gmail.com>
2024-11-17 00:15:42 +0900

    run length encoding, rle

--- abc/abc380/c.cpp
+++ abc/abc380/c.cpp
@@ -6,58 +6,32 @@
 int main() {
     int n, k;
     std::cin >> n >> k;
-    k--;
 
     std::string s;
     std::cin >> s;
 
-    std::vector<std::pair<int, int>> intervals;
-    std::pair<int, int> interval{-1, -1};
-    bool is_interval = false;
-    for (int i = 0; i < n; i++) {
-        if (!is_interval && s[i] == '1') {
-            interval.first = i;
-            is_interval = true;
-        }
-        if (is_interval && s[i] == '0') {
-            interval.second = i - 1;
-            intervals.push_back(interval);
-            interval = {-1, -1};
-            is_interval = false;
-        }
-    }
-    if (is_interval) {
-        interval.second = n - 1;
-        intervals.push_back(interval);
-    }
-
-    int i = 0;
-    int interval_idx = -1;
-    auto [kth_head, kth_tail] = intervals[k];
-    is_interval = false;
-    while (i < n) {
-        std::cout << s[i];
-        if (!is_interval && s[i] == '1') {
-            interval_idx++;
-            is_interval = true;
-        }
-        if (is_interval && s[i] == '0') {
-            is_interval = false;
-        }
-
-        if (interval_idx == k - 1 && is_interval) {
-            if (i != n - 1 && s[i + 1] == '1') {
-                i++;
+    auto rle = [](const std::string &s) {
+        std::vector<std::pair<char, int>> res;
+        for (const char &c : s) {
+            if (res.empty() || res.back().first != c) {
+                res.emplace_back(c, 1);
                 continue;
             }
-            std::cout << std::string(kth_tail - kth_head + 1, '1');
-            std::cout << std::string(kth_head - i - 1, '0');
-            i = kth_tail + 1;
-            interval_idx++;
-            continue;
+            res.back().second++;
         }
-        i++;
+        return res;
+    };
+
+    auto encoded_s = rle(s);
+    int chunk_idx = 0;
+    for (std::size_t i = 0; i < encoded_s.size(); i++) {
+        const auto &[c, _] = encoded_s[i];
+        if (c == '1') chunk_idx++;
+        if (chunk_idx == k) std::swap(encoded_s[i - 1], encoded_s[i]);
     }
+
+    for (const auto &[c, num] : encoded_s) std::cout << std::string(num, c);
     std::cout << std::endl;
+
     return 0;
 }
