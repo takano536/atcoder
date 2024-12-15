180e86d44f1caced75201cde236ef84e9aad62bd
takao536 <ohnishi.takanori@gmail.com>
2024-12-15 17:16:24 +0900

    bit full search

--- abc/abc384/c.cpp
+++ abc/abc384/c.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <ranges>
 #include <string>
 #include <utility>
 #include <vector>
@@ -11,7 +12,7 @@ int main() {
     std::array<int, N> scores;
     for (auto &score : scores) std::cin >> score;
 
-    std::vector<std::pair<int, std::string>> prob_ids;
+    std::vector<std::pair<int, std::string>> standings;
     for (int bit = 0; bit < (1 << N); bit++) {
         std::vector<int> probs;
         for (int i = 0; i < N; i++) {
@@ -24,10 +25,10 @@ int main() {
         std::string id;
         for (const auto &i : probs) id += 'A' + i;
 
-        prob_ids.push_back(std::make_pair(-total, id));
+        standings.push_back(std::make_pair(-total, id));
     }
 
-    std::ranges::sort(prob_ids);
-    for (const auto &[_, ans] : prob_ids) std::cout << ans << std::endl;
+    std::ranges::sort(standings);
+    for (const auto &ans : std::views::values(standings)) std::cout << ans << std::endl;
     return 0;
 }
